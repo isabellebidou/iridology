4ee237550c21036716be9baf4226d1563488b98d
isabelle <isa.bidou@gmail.com>
2022-12-12 14:43:39 +0100

    adding redirect domain key and production routing

--- index.js
+++ index.js
@@ -20,6 +20,16 @@ app.use(passport.initialize());
 app.use(passport.session());
 
 require('./routes/authRoutes')(app);
+if (process.env.NODE_ENV == 'production') {
+  // express will serve up production assets
+  app.use(express.static('client/build'));
+  
+  // express will serve up the index.html file if it doesn't recognize the routes
+  const path = require('path');
+  app.get('*', (req, res)=> {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+  });
+  }
 
 const PORT = process.env.PORT || 7000;
 app.listen(PORT);
