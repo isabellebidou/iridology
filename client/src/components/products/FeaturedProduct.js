3bd4ef362a52c825eb91052aa5c4e2eb975b179d
isabelle bidou <isa.bidou@gmail.com>
2023-04-20 19:21:39 +0200

    cleaned icons, changed pierre link, changed me picture

--- client/src/components/products/FeaturedProduct.js
+++ client/src/components/products/FeaturedProduct.js
@@ -9,7 +9,7 @@ function FeaturedProduct() {
         title: ['ÅtayatÉ - La Pierre de Cohérence® '],
         id: ['stone'],
         link: [
-            'https://www.atayate.com/products/pierre-de-coherence?bg_ref=Poap8MPk0Z'
+            'https://www.atayate.com/collections/pierres-de-coherence'
         ],
         description: [
             'The Pierre de Coherence® (Consistency Stone) is a technology of Light and Coherence of a Stone to the Field of Universal Coherence, which was de-covered and implemented by Marc Boucher de Lignon 30 years ago.The Pierre de Coherence® is a unique technology that validates the latest discoveries in quantum physics and epigenetics (consistent with the work of Dr. Bruce Lipton).   Thus, this technology put at the service of collective harmony, allows everyone to be consistent with himself and with the Universe. Stone preparation involves a careful selection to promote balance and harmony in the users body.'
