fcbdae5e16e42b28a79491ed7ec77ee550dbdbae
isabelle bidou <isa.bidou@gmail.com>
2023-06-08 12:01:33 +0200

    implemented coupon component

---
+++ client/src/components/Coupon.js
@@ -0,0 +1,39 @@
+import React from "react";
+import { AiOutlineCopy, AiFillCopy } from "react-icons/ai";
+
+class Coupon extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            copied: false
+        };
+    }
+    copyText = () => {
+        // Get the text from the span element
+        const copyText = document.querySelector(".coupon");
+
+        // Copy the text to the clipboard
+        navigator.clipboard.writeText(copyText.textContent);
+
+        // Set the copied state to true
+        this.setState({ copied: true });
+    };
+
+    render() {
+        const { copied } = this.state;
+        return (
+            <div>
+                <span className="coupon" onClick={this.copyText}>
+                    {this.props.text}
+                    {copied ? (
+                        <AiFillCopy style={{ color: "#7f5f87" }} />
+                    ) : (
+                        <AiOutlineCopy style={{ color: "#7f5f87" }} />
+                    )}
+                </span>
+            </div>
+        );
+    }
+}
+export default Coupon;
--- client/src/components/Shop.js
+++ client/src/components/Shop.js
@@ -5,12 +5,14 @@ import Glandular from './products/Glandular';
 import MorseFormulas from './products/MorseFormulas';
 import AmazonProducts from './products/AmazonProducts';
 import { Helmet } from 'react-helmet-async';
+import Coupon from "./Coupon";
 
 
 
 
 
 const Shop = () => {
+
     return (<>
         <Helmet>
             <title>My affiliate health products list</title>
@@ -35,7 +37,7 @@ const Shop = () => {
             </div>
             <p className="disclaimerp">Disclaimer: The content provided on this page is for informational and recreational purposes only. It is not intended to be a substitute for professional medical advice, diagnosis, or treatment. Only a licensed medical doctor can diagnose and prescribe medication or treatment. Any information provided on this page should not be relied upon for making decisions about your health or well-being. Before considering any treatment, it is essential to consult with your doctor or therapist to ensure that it is safe and suitable for your specific needs and medical history.</p>
             <fieldset>
-                
+
                 <div>
                     <img className="affiliatelogo" src="https://cdn.shopify.com/s/files/1/0655/4094/8233/files/atayate_droit_corrige.png?v=1676308709&width=200" alt="atayate" loading="lazy" title="atayate logo" ></img>
                     <p className="itemp">
@@ -48,7 +50,7 @@ const Shop = () => {
             </fieldset>
 
             <fieldset>
-                
+
                 <div>
                     <img className="affiliatelogo" src="https://affiliation.anastore.com/en/images/logo_header.png" alt="anastore logo" loading="lazy" title="anastore logo" ></img>
 
@@ -63,17 +65,20 @@ const Shop = () => {
             </fieldset>
 
             <fieldset>
-               
+
 
                 <div>
                     <img className=" affiliatelogo" src="https://cdn.shopify.com/s/files/1/0725/6913/files/dr-morses-herbal-health-logo_200x.png?v=1613745566" alt="dr morse logo" loading="lazy" title="dr morse logo" ></img>
 
                     <p className="itemp">
-                        You can use the coupon : <span className="coupon">AF.UPPROMOTE.COM</span> to order from Dr. Morse's website directly. Please visit : 
+                        You can use the Coupon :
+                        <Coupon
+                            text='AF.UPPROMOTE.COM'
+                        ></Coupon> to order from Dr. Morse's website directly. Please visit :
                         <a href="https://drmorsesherbalhealthclub.com/">Dr Morse's herbal health club</a>
-                        <br /> 
+                        <br />
                         Alternatively  you can click on the links below to order.
-                        <br /> 
+                        <br />
                         Beware, the formulas are shipping from the US. International shipping costs DO NOT include Import fees, Duties, or Taxes. Not shipping to all countries.
                     </p></div>
 
@@ -84,7 +89,7 @@ const Shop = () => {
             </fieldset>
 
             <fieldset>
-                
+
 
                 <div>
                     <img className=" affiliatelogo" src="https://www.drrons.com/images/Logo-Header.png" alt="dr ron logo" loading="lazy" title="dr ron logo" ></img>
@@ -97,20 +102,20 @@ const Shop = () => {
                 <Glandular />
 
             </fieldset>
-            
-               
 
-                <div>
-                    <img className=" affiliatelogo" src="https://m.media-amazon.com/images/G/01/AdProductsWebsite/images/AUX/03_available_at_amazon_logo_stacked_RGB_SQUID._TTW_.png" alt="available at Amazon logo" loading="lazy" title="available at Amazon logo" ></img>
 
-                    <p className="itemp">
-                        This is an affiliate program. I will receive a commission if you buy any of the articles via the links below.
-                    </p></div>
-                <span id="amazon" >
-                </span>
-                <AmazonProducts />
 
-           
+            <div>
+                <img className=" affiliatelogo" src="https://m.media-amazon.com/images/G/01/AdProductsWebsite/images/AUX/03_available_at_amazon_logo_stacked_RGB_SQUID._TTW_.png" alt="available at Amazon logo" loading="lazy" title="available at Amazon logo" ></img>
+
+                <p className="itemp">
+                    This is an affiliate program. I will receive a commission if you buy any of the articles via the links below.
+                </p></div>
+            <span id="amazon" >
+            </span>
+            <AmazonProducts />
+
+
 
 
         </div>
