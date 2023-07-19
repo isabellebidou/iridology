972f0d35b6d54c2b4fb47fd6a58771b703b2b426
isabelle bidou <isa.bidou@gmail.com>
2023-07-19 11:47:36 +0200

    added triphala

--- client/src/components/products/AmazonProducts.js
+++ client/src/components/products/AmazonProducts.js
@@ -84,10 +84,10 @@ function AmazonProducts() {
                 <h2>Exemple de cure detox en trois étapes sur 6 semaines</h2>
                 <ul className='ulp'>
                     <li>Etape 1 pour les semaines 1 et 2 :
-                    Argile verte loin des repas, cascara sagrada, AntiPar qui contient de l’absinthe, du clou de girofle, de la cannelle, du curcuma et de l’ortie.
+                    Argile verte loin des repas, cascara sagrad ou triphala, AntiPar qui contient de l’absinthe, du clou de girofle, de la cannelle, du curcuma et de l’ortie.
                     </li>
                     <li>Etape 2 pour les semaines 3 et 4 :
-                    continuer argile verte loin des repas, cascara sagrada. Ajouter: comprimés ou  capsules d’ortie et/ou pissenlit et/ou chiendent</li>
+                    continuer argile verte loin des repas, cascara sagrada ou triphala. Ajouter: comprimés ou  capsules d’ortie et/ou pissenlit et/ou chiendent</li>
                     <li>Etape 3 pour les semaines 5 et 6 :
                     comprimés ou  capsules de :
                     gratteron et/ou plantain et/ou souci officinal et/ou fenugrec</li>
